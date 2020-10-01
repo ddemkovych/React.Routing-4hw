79b1a90e1495b054899b3518ff0c0159c838b5d0
Dmytro <ddemkovych4@gmail.com>
2020-10-01 17:36:32 +0300

    Updates

---
+++ precache-manifest.40142e5dd82dfb43cee0f9758185e078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5dbc03e6214a3c8e2f5dd425ed17235",
+    "url": "/React.Routing-4hw/index.html"
+  },
+  {
+    "revision": "d8971b6b2425ef53d3b0",
+    "url": "/React.Routing-4hw/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0cca004cdb1f66bdf4e6",
+    "url": "/React.Routing-4hw/static/js/2.c9efceb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React.Routing-4hw/static/js/2.c9efceb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8971b6b2425ef53d3b0",
+    "url": "/React.Routing-4hw/static/js/main.eebdfe6d.chunk.js"
+  },
+  {
+    "revision": "da4dff52984c284a5e17",
+    "url": "/React.Routing-4hw/static/js/runtime-main.a1a8cf1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React.Routing-4hw/static/media/logo.5d5d9eef.svg"
+  }
+]);
